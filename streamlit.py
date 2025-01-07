8f28df2b4d8698b3cd4b2545fd2b7841f77a03c5
saurabh4060 <124861320+saurabh4060@users.noreply.github.com>
2025-01-07 15:49:37 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -15,7 +15,7 @@ if f1 is not None:
     st.write(filename)
     df = pd.read_csv(filename,encoding="ISO-8859-1")
 else:
-    df= pd.read_csv("C:/Users/ROCKSTAR/Desktop/streamlit/Superstore.csv")
+    st.error("Please upload a file.")
 
 
 col1,col2 = st.columns((2))
